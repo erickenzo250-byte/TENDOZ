8de92e5d31ccab064111918d1a8a2324039ee789
erickenzo250-byte <erickenzo250@gmail.com>
2025-09-21 11:37:20 +0300

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -2,12 +2,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# SQLite instead of Postgres
 DATABASE_URL = "sqlite:///./dating.db"
 
 engine = create_engine(
     DATABASE_URL, connect_args={"check_same_thread": False}
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
 Base = declarative_base()
